726e09fd9c2fdb11e1d8dfa835c9c60859f915cc
luissgrimaldi <117244567+luissgrimaldi@users.noreply.github.com>
2022-12-01 15:29:09 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ headerBtn.addEventListener("click", function(){
     let headerNav=document.getElementById("header__nav");
     headerNav.classList.toggle("header__nav--toggle")
 });
+
+let profile = document.querySelector(".header__li-user");
+profile.addEventListener("click",function(e){
+    e.preventDefault();
+    let profileMenu = document.querySelector(".header__li-user__ul");
+
+    profileMenu.classList.toggle("header__li-user__ul--toggle");
+    if(profile.style.transform == "scale(1)"){
+        profile.removeAttribute("style")
+    }else{
+        profile.style.transform = "scale(1)"
+    }
+})
