5f12089064ed0a9cde9b4484a30f29db1de84b4e
luissgrimaldi <117244567+luissgrimaldi@users.noreply.github.com>
2023-02-14 02:14:40 -0300

    Add files via upload

--- backend/buscador.php
+++ backend/buscador.php
@@ -1,21 +1,30 @@
 <?php
 include 'connect.php';
 
-$campo = $_POST['campo'];
+$campo = trim($_POST['campo']);
 
-$sentencia = $connect->prepare("SELECT * FROM `wp_propiedades` WHERE `calle` LIKE ?") or die('query failed');
-$sentencia->execute([$campo . '%']);
+$sentencia = $connect->prepare("SELECT * FROM `wp_propiedades` WHERE `calle` LIKE ? OR `referencia_interna` LIKE ?") or die('query failed');
+$sentencia->execute([$campo . '%', $campo . '%']);
+$sentencia2 = $connect->prepare("SELECT * FROM `wp_contactos` WHERE `nombre` + `apellido` LIKE ?") or die('query failed');
+$sentencia2->execute(['%' . $campo . '%']);
 
 $html = "";                   
 while($row = $sentencia->fetch(PDO::FETCH_ASSOC)){
 
-    $html .= "<li>".$row['calle']." ".$row['referencia_interna']."</li>";
+    $html .= '<li><a href="editarpropiedad.php?ref='.$row["referencia_interna"].'">'.$row["calle"]." ".$row["referencia_interna"].'</a></li>';
 
 };      
+                 
+while($row = $sentencia2->fetch(PDO::FETCH_ASSOC)){
 
+    $html .= '<li>'.$row["nombre"]." ".$row["apellido"].'</li>';
+
+};      
+ 
 echo json_encode($html, JSON_UNESCAPED_UNICODE);
 
 ?>
 
 
 
+ 
